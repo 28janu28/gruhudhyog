5abc610677e496c8a664a0eeb9c1222ae306cb7e
28janu28 <janu.ranpara@gmail.com>
2021-08-21 15:39:18 +0530

    category table is added in admin panel

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,13 @@
+<?php
+require_once('config.php');
+
+$qery_for_fetching_category_dataset = "SELECT id, name, created_at, created_by, updated_at, updated_by FROM category";
+$result = $conn->query($qery_for_fetching_category_dataset);
+$category_dataset = [];
+if ($result->num_rows > 0) {
+    $category_dataset = $result->fetch_all(MYSQLI_ASSOC);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,7 +15,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Admin Panel</title>
-        <link href="css/datatable.css" rel="stylesheet" />
+        <!-- <link href="css/datatable.css" rel="stylesheet" /> -->
+        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
@@ -33,11 +44,11 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                                Category
                             </a>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <!-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Layouts
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
@@ -47,7 +58,7 @@
                                     <a class="nav-link" href="layout-static.html">Static Navigation</a>
                                     <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
                                 </nav>
-                            </div>
+                            </div> -->
                             
                         </div>
                     </div>
@@ -56,48 +67,41 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Tables</h1>
+                        <h1 class="mt-4">Category</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="">Category</a></li>
                             <li class="breadcrumb-item active">Tables</li>
                         </ol>
 						
                         <div class="card mb-4">
                             <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                DataTable Example
+                                <!-- <i class="fas fa-table me-1"></i> -->
+                                <a class="btn btn-primary" hrer="">Add Category</a>
                             </div>
                             <div class="card-body">
-                                <table id="datatablesSimple">
+                                <table id="categoryTable">
                                     <thead>
                                         <tr>
+                                            <th>ID</th>
                                             <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th>Created Date</th>
+                                            <th>Created By</th>
+                                            <th>Updated Date</th>
+                                            <th>Updated By</th>
                                         </tr>
                                     </thead>
-									
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
-                                        </tr>
-                                    </tbody>
+                                    <?php if(!empty($category_dataset)) { ?>
+                                        <?php foreach($category_dataset as $category) { ?>
+                                            <tr>
+                                                <td><?php echo $category['id']; ?></td>
+                                                <td><?php echo $category['name']; ?></td>
+                                                <td><?php echo $category['created_at']; ?></td>
+                                                <td><?php echo $category['created_by']; ?></td>
+                                                <td><?php echo $category['updated_at']; ?></td>
+                                                <td><?php echo $category['updated_by']; ?></td>
+                                            </tr>
+                                        <?php } ?>
+                                    <?php } ?>
                                 </table>
                             </div>
                         </div>
@@ -107,7 +111,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; 2021 Developed by : Harshad Fefar</div>
+                            <div class="text-muted">Copyright &copy; 2021 Developed by : Janvi Ranpara</div>
                         </div>
                     </div>
                 </footer>
@@ -115,7 +119,15 @@
         </div>
         <script src="js/bootstrap.js"></script>
         <script src="js/scripts.js"></script>
-        <script src="js/datatable.js"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        <!-- <script src="js/datatable.js"></script>
+        <script src="js/datatables-simple-demo.js"></script> -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript" src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#categoryTable').DataTable();
+            });
+        </script>
+        
     </body>
 </html>
